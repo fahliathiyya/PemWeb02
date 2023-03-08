a893f53281aa85d2e3d5f52b043368c506cd636e
Fahlia Athiyya Marva <68423799+fahliathiyya@users.noreply.github.com>
2023-03-08 21:07:06 +0700

    Delete Praktikum03 directory

--- Praktikum03/dowhile_looping.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nomor_looping = 0;
-do {
-   echo "ini adalah looping ke-" . $nomor_looping ."<br>";
-   $nomor_looping++;
-} while ($nomor_looping < 10);
-?>
--- Praktikum03/foreach_looping.php
+++
@@ -1,7 +0,0 @@
-<?php
-$array = ['kucing', 'anjing', 'kuda', 'kupu-kupu', 'semut'];
-
-foreach ($array as $key => $value) {
-    echo "index ke- $key adalah $value <br>";
-}
-?>
